de681e95aad88d060c8a04e4b436b4064c5aa429
Chiranjeevi Pandey <chiran604@gmail.com>
2021-11-08 15:49:51 +0545

    add: add new utility functions

--- app/src/main/java/com/gbsoft/merosim/utils/Utils.java
+++ app/src/main/java/com/gbsoft/merosim/utils/Utils.java
@@ -10,28 +10,44 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Last modified: 2021/05/31
+ * Last modified: 2021/10/26
  */
 
 package com.gbsoft.merosim.utils;
 
 import android.annotation.SuppressLint;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 
 import androidx.annotation.StringRes;
+import androidx.camera.core.ExperimentalGetImage;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.core.text.HtmlCompat;
 
+import com.gbsoft.easyussd.UssdService;
 import com.gbsoft.merosim.R;
+import com.gbsoft.merosim.data.Sim;
 import com.gbsoft.merosim.intermediaries.PrefsUtils;
+import com.gbsoft.merosim.ui.recharge.OnTextRecognizedListener;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Utils {
 
@@ -50,7 +66,7 @@ public class Utils {
         return PrefsUtils.getDefaultSharedPrefs(context).getBoolean("key_vibrate", true);
     }
 
-    // send 0 for ntc, 1 for ncell and 2 for smartcell
+    // send 0 for ntc, 1 for ncell and 2 for smart cell
     public static int getSimColor(Resources res, int sim) {
         boolean isNightMode = isNightMode(res);
         switch (sim) {
@@ -102,7 +118,7 @@ public class Utils {
         Context context = anchor.getContext();
         new MaterialAlertDialogBuilder(context)
                 .setTitle(R.string.info_txt)
-                .setIcon(R.drawable.ic_baseline_info_24)
+                .setIcon(R.drawable.ic_round_help_center_24)
                 .setMessage(HtmlCompat.fromHtml(context.getString(info), 0).toString())
                 .setCancelable(true)
                 .setPositiveButton(R.string.dialog_positive_btn_txt, (dialog, id) ->
@@ -110,4 +126,77 @@ public class Utils {
                 .show();
     }
 
+    @ExperimentalGetImage
+    public static void recognizeText(String simName, InputImage image, OnTextRecognizedListener listener) {
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        recognizer.process(image)
+                .addOnSuccessListener(text -> {
+                    Log.d("Analyse", text.getText());
+
+                    String scanned = getPinFromLine(simName, text.getText());
+                    if (scanned.length() == 16)
+                        listener.onTextRecognized(scanned);
+                    else
+                        listener.onRecognizationFailed(new Exception("Couldn't scan the pin!!"));
+                })
+                .addOnFailureListener(listener::onRecognizationFailed);
+    }
+
+    private static String getPinFromLine(Text.Line line) {
+        StringBuilder pinStr = new StringBuilder();
+        char[] lineChars = line.getText().toCharArray();
+        for (char c : lineChars) {
+            if (pinStr.length() < 16 && Character.isDigit(c)) {
+                pinStr.append(c);
+            }
+        }
+        return pinStr.toString();
+    }
+
+    public static String getPinFromLine(String simName, String scannedText) {
+        Pattern pinPattern = null;
+        switch (simName) {
+            case Sim.NAMASTE:
+                pinPattern = Pattern.compile("(\\d{5}.?\\d{6}.?\\d{5})");
+                break;
+            case Sim.NCELL:
+                pinPattern = Pattern.compile("(\\d{4}.?\\d{4}.?\\d{4}.?\\d{4})");
+                break;
+            case Sim.SMART_CELL:
+                pinPattern = Pattern.compile("\\d{4}.?\\d{5}.?\\d{4}");
+                break;
+        }
+
+        Matcher pinMatcher = pinPattern.matcher(scannedText);
+
+        if (pinMatcher.find())
+            return pinMatcher.group().replaceAll("\\D", "");
+        else
+            return "";
+    }
+
+    public static boolean isAccessibilityServiceEnabled(Context context) {
+        ComponentName expectedComponentName = new ComponentName(context, UssdService.class);
+
+        String enabledServicesSetting = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+        if (enabledServicesSetting == null)
+            return false;
+
+        TextUtils.SimpleStringSplitter colonSplitter = new TextUtils.SimpleStringSplitter(':');
+        colonSplitter.setString(enabledServicesSetting);
+
+        while (colonSplitter.hasNext()) {
+            String componentNameString = colonSplitter.next();
+            ComponentName enabledComponentName = ComponentName.unflattenFromString(componentNameString);
+
+            if (enabledComponentName != null && enabledComponentName.equals(expectedComponentName))
+                return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isOverlayServiceEnabled(Context context) {
+        return Settings.canDrawOverlays(context);
+    }
 }
